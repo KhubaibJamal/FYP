47a7f4ff8fd31f95404f058e6c0df64695bff041
Khubaib Jamal <khubaib.j18@gmail.com>
2024-01-22 23:13:51 +0500

    button: adding outline rounded button

---
+++ lib/buttons/outline_rounded_button_4.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class OutlineRoundedButton4 extends StatelessWidget {
+  const OutlineRoundedButton4({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlinedButton(
+      onPressed: () {},
+      style: ButtonStyle(
+        side: MaterialStateProperty.all(
+          const BorderSide(color: Color(0xff2749FD), width: 1.4),
+        ),
+        padding: MaterialStateProperty.all(
+          const EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+        ),
+        shape: MaterialStateProperty.all(
+          const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(40),
+            ),
+          ),
+        ),
+      ),
+      child: const Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            "Sign Out",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.w300,
+              color: Color(0xff2749FD),
+            ),
+          ),
+          Icon(Icons.arrow_forward, color: Color(0xff2749FD))
+        ],
+      ),
+    );
+  }
+}
