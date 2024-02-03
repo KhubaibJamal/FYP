d3a4ff8451d727ae2837adbd30a0add0c745ceb3
Khubaib Jamal <khubaib.j18@gmail.com>
2024-02-04 01:12:57 +0500

    search box: simple search

--- lib/home/search_bars.dart
+++ lib/home/search_bars.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:fyp/search%20bar/middle_text_search_bar2.dart';
 import 'package:fyp/search%20bar/search_bar1.dart';
 
 class SearchBars extends StatelessWidget {
   SearchBars({super.key});
 
   final TextEditingController controllerOne = TextEditingController();
+  final TextEditingController controllerTwo = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -17,6 +19,11 @@ class SearchBars extends StatelessWidget {
             children: [
               SearchBar1(
                 textController: controllerOne,
+                hintText: "Search Here",
+              ),
+              const SizedBox(height: 10),
+              MiddleTextSearchBar2(
+                searchController: controllerTwo,
                 hintText: "Search Here",
               ),
               const SizedBox(height: 10),
---
+++ lib/search bar/middle_text_search_bar2.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class MiddleTextSearchBar2 extends StatelessWidget {
+  final TextEditingController searchController;
+  final String hintText;
+
+  const MiddleTextSearchBar2(
+      {required this.searchController, required this.hintText, Key? key})
+      : super(
+          key: key,
+        );
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      decoration: BoxDecoration(boxShadow: [
+        BoxShadow(
+            offset: const Offset(12, 26),
+            blurRadius: 50,
+            spreadRadius: 0,
+            color: Colors.grey.withOpacity(.1)),
+      ]),
+      child: TextField(
+        controller: searchController,
+        textAlign: TextAlign.center,
+        onChanged: (value) {},
+        style: const TextStyle(fontSize: 14),
+        decoration: InputDecoration(
+          // prefixIcon: Icon(Icons.email),
+          prefixIcon:
+              const Icon(Icons.search, size: 20, color: Color(0xffFF5A60)),
+          filled: true,
+          fillColor: Colors.white,
+          hintText: hintText,
+          hintStyle: TextStyle(color: Colors.black.withOpacity(.75)),
+          contentPadding:
+              const EdgeInsets.symmetric(vertical: 0.0, horizontal: 20.0),
+          border: const OutlineInputBorder(
+            borderSide: BorderSide(color: Color(0xffFF5A60), width: 2.0),
+            borderRadius: BorderRadius.all(Radius.circular(15.0)),
+          ),
+          enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Color(0xffFF5A60), width: 2.0),
+            borderRadius: BorderRadius.all(Radius.circular(15.0)),
+          ),
+          focusedBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Color(0xffFF5A60), width: 2.0),
+            borderRadius: BorderRadius.all(Radius.circular(15.0)),
+          ),
+        ),
+      ),
+    );
+  }
+}
