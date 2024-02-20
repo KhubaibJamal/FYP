f86135bf47dac8a291e75fbf9f5f72c27997e5f9
Khubaib Jamal <khubaib.j18@gmail.com>
2024-02-21 00:16:08 +0500

    refactor textfield 9

--- lib/text fields/input_text_field9.dart
+++ lib/text fields/input_text_field9.dart
@@ -11,24 +11,32 @@ class InputTextField9 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      controller: controller,
-      style: const TextStyle(fontSize: 14, color: Colors.white),
-      decoration: InputDecoration(
-        suffixIcon: const Icon(Icons.done, color: Colors.white),
-        filled: true,
-        fillColor: const Color(0xFF3d3d47),
-        hintText: hintText,
-        labelText: hintText,
-        labelStyle: TextStyle(color: Colors.white.withOpacity(0.75)),
-        hintStyle: TextStyle(color: Colors.white.withOpacity(0.75)),
-        contentPadding: const EdgeInsets.symmetric(horizontal: 10.0),
-        border: const UnderlineInputBorder(
-            borderSide: BorderSide(color: Colors.white)),
-        focusedBorder: const UnderlineInputBorder(
-            borderSide: BorderSide(color: Colors.white)),
-        enabledBorder: const UnderlineInputBorder(
-            borderSide: BorderSide(color: Colors.white)),
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      height: 80,
+      width: MediaQuery.of(context).size.width,
+      color: const Color(0xFF363640),
+      child: Center(
+        child: TextField(
+          controller: controller,
+          style: const TextStyle(fontSize: 14, color: Colors.white),
+          decoration: InputDecoration(
+            suffixIcon: const Icon(Icons.done, color: Colors.white),
+            filled: true,
+            fillColor: const Color(0xFF3d3d47),
+            hintText: hintText,
+            labelText: hintText,
+            labelStyle: TextStyle(color: Colors.white.withOpacity(0.75)),
+            hintStyle: TextStyle(color: Colors.white.withOpacity(0.75)),
+            contentPadding: const EdgeInsets.symmetric(horizontal: 10.0),
+            border: const UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white)),
+            focusedBorder: const UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white)),
+            enabledBorder: const UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white)),
+          ),
+        ),
       ),
     );
   }
