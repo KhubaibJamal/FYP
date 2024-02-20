864869332125e7a0cab69ad7a7d7c6b41d56f7ca
Khubaib Jamal <khubaib.j18@gmail.com>
2024-02-21 01:29:30 +0500

    refactor code

--- lib/text fields/input_text_field12.dart
+++ lib/text fields/input_text_field12.dart
@@ -16,45 +16,49 @@ class InputTextField12 extends StatelessWidget {
       height: 80,
       width: MediaQuery.of(context).size.width,
       color: const Color(0xFFff9186),
-      child: Center(
-        child: TextField(
-          controller: controller,
-          style: const TextStyle(fontSize: 14, color: Color(0xFFff9186)),
-          decoration: InputDecoration(
-            suffixIcon: Container(
-              margin: const EdgeInsets.only(right: 5),
-              height: 40,
-              width: 60,
-              decoration: BoxDecoration(
-                color: const Color(0xFFff9186),
-                borderRadius: BorderRadius.circular(25),
-              ),
-              child: const Center(
-                child: Text(
-                  "Send",
-                  style: TextStyle(color: Colors.white),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: controller,
+              style: const TextStyle(fontSize: 14, color: Color(0xFFff9186)),
+              decoration: InputDecoration(
+                suffixIcon: Container(
+                  margin: const EdgeInsets.only(right: 8, top: 5, bottom: 5),
+                  height: 40,
+                  width: 60,
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFff9186),
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      "Send",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                filled: true,
+                fillColor: Colors.white,
+                hintText: hintText,
+                hintStyle: const TextStyle(color: Color(0xFFff9186)),
+                contentPadding: const EdgeInsets.symmetric(horizontal: 10.0),
+                border: const OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.transparent),
+                  borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                ),
+                enabledBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.transparent),
+                  borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                ),
+                focusedBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.transparent),
+                  borderRadius: BorderRadius.all(Radius.circular(25.0)),
                 ),
               ),
             ),
-            filled: true,
-            fillColor: Colors.white,
-            hintText: hintText,
-            hintStyle: const TextStyle(color: Color(0xFFff9186)),
-            contentPadding: const EdgeInsets.symmetric(horizontal: 10.0),
-            border: const OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.transparent),
-              borderRadius: BorderRadius.all(Radius.circular(25.0)),
-            ),
-            enabledBorder: const OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.transparent),
-              borderRadius: BorderRadius.all(Radius.circular(25.0)),
-            ),
-            focusedBorder: const OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.transparent),
-              borderRadius: BorderRadius.all(Radius.circular(25.0)),
-            ),
           ),
-        ),
+        ],
       ),
     );
   }
