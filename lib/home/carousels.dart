9e7e34fbec04aa4db4a18d43cff2bb9cd3fa5270
Khubaib Jamal <khubaib.j18@gmail.com>
2024-06-04 00:48:13 +0500

    widget:swap carousel design

--- lib/home/carousels.dart
+++ lib/home/carousels.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fyp/carousel/carousel1.dart';
 import 'package:fyp/carousel/carousel2.dart';
+import 'package:fyp/carousel/carousel3.dart';
 
 class Carousels extends StatelessWidget {
   const Carousels({super.key});
@@ -8,11 +9,13 @@ class Carousels extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(),
       body: Column(
         children: [
-          const Expanded(child: Carousel1()),
-          Expanded(child: Carousel2()),
+          // const Expanded(child: Carousel1()),
+          // Expanded(child: Carousel2()),
+          Expanded(child: Carousel3()),
         ],
       ),
     );
