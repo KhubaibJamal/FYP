e5c0b2d0c1d20ca04abccd1e5f1444efb5e85055
Khubaib Jamal <khubaib.j18@gmail.com>
2024-06-06 23:20:48 +0500

    widget:design horizontal list view for food card

--- lib/home/listviews.dart
+++ lib/home/listviews.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fyp/list%20view/list_view1.dart';
+import 'package:fyp/list%20view/list_view2.dart';
 
 class ListViews extends StatelessWidget {
   const ListViews({super.key});
@@ -8,11 +9,13 @@ class ListViews extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: SingleChildScrollView(
+      body: const SingleChildScrollView(
         child: Column(
           children: [
             ListView1(),
-            const SizedBox(height: 10),
+            SizedBox(height: 10),
+            ListView2(),
+            SizedBox(height: 10),
           ],
         ),
       ),
