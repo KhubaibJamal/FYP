99db03278135c8bb32feb59e58b037970947ca14
Khubaib Jamal <khubaib.j18@gmail.com>
2024-06-23 00:20:59 +0500

    widget:simple linear progres indicator

--- lib/home/loading_indicators.dart
+++ lib/home/loading_indicators.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fyp/loading%20indicator/loading_indicator1.dart';
 import 'package:fyp/loading%20indicator/loading_indicator2.dart';
+import 'package:fyp/loading%20indicator/loading_indicator3.dart';
 
 class LoadingIndicators extends StatelessWidget {
   const LoadingIndicators({super.key});
@@ -11,19 +12,23 @@ class LoadingIndicators extends StatelessWidget {
       backgroundColor: const Color(0xFFebebeb),
       appBar: AppBar(),
       body: const Center(
-        child: Column(
-          children: [
-            SizedBox(height: 20),
-            LoadingIndicator1(
-              currentDotColor: Color(0xFFC80036),
-              defaultDotColor: Color(0xFFFF6969),
-              numDots: 10,
-            ),
-            SizedBox(height: 20),
-            LoadingIndicator2(),
-            SizedBox(height: 20),
-            SizedBox(height: 20),
-          ],
+        child: Padding(
+          padding: EdgeInsets.only(left: 15, right: 15),
+          child: Column(
+            children: [
+              SizedBox(height: 20),
+              LoadingIndicator1(
+                currentDotColor: Color(0xFFC80036),
+                defaultDotColor: Color(0xFFFF6969),
+                numDots: 10,
+              ),
+              SizedBox(height: 20),
+              LoadingIndicator2(),
+              SizedBox(height: 20),
+              LoadingIndicator3(),
+              SizedBox(height: 20),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/loading indicator/loading_indicator3.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LoadingIndicator3 extends StatelessWidget {
+  const LoadingIndicator3({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const LinearProgressIndicator(color: Color(0xFF686D76));
+  }
+}
