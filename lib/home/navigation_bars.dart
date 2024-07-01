5f59f1ba0160baa602852732a9fb2bb9b5338c72
Khubaib Jamal <khubaib.j18@gmail.com>
2024-07-02 00:11:09 +0500

    widget:curve bottom nav bar

--- lib/home/navigation_bars.dart
+++ lib/home/navigation_bars.dart
@@ -3,6 +3,7 @@ import 'package:fyp/navigation%20bar/nav_bar1.dart';
 import 'package:fyp/navigation%20bar/nav_bar2.dart';
 import 'package:fyp/navigation%20bar/nav_bar3.dart';
 import 'package:fyp/navigation%20bar/nav_bar4.dart';
+import 'package:fyp/navigation%20bar/nav_bar5.dart';
 
 class NAvigationBars extends StatelessWidget {
   const NAvigationBars({super.key});
@@ -24,6 +25,7 @@ class NAvigationBars extends StatelessWidget {
               SizedBox(height: 20),
               NavBar4(),
               SizedBox(height: 20),
+              NavBar5(),
               SizedBox(height: 20),
             ],
           ),
---
+++ lib/navigation bar/nav_bar5.dart
@@ -0,0 +1,158 @@
+import 'package:flutter/material.dart';
+
+class NavBar5 extends StatefulWidget {
+  const NavBar5({Key? key}) : super(key: key);
+
+  @override
+  _NavBar5State createState() => _NavBar5State();
+}
+
+class _NavBar5State extends State<NavBar5> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    double height = 56;
+
+    const primaryColor = Colors.orange;
+    const secondaryColor = Colors.black54;
+    const backgroundColor = Colors.white;
+
+    return BottomAppBar(
+      color: const Color(0xFF002379),
+      elevation: 0,
+      child: Stack(
+        children: [
+          CustomPaint(
+            size: Size(size.width, height + 6),
+            painter: BottomNavCurvePainter(backgroundColor: backgroundColor),
+          ),
+          Center(
+            heightFactor: 0.6,
+            child: FloatingActionButton(
+              backgroundColor: primaryColor,
+              elevation: 0.1,
+              onPressed: () {},
+              child: const Icon(Icons.shopping_basket),
+            ),
+          ),
+          SizedBox(
+            height: height,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                NavBarIcon(
+                  text: "Home",
+                  icon: Icons.home_outlined,
+                  selected: true,
+                  onPressed: () {},
+                  defaultColor: secondaryColor,
+                  selectedColor: primaryColor,
+                ),
+                NavBarIcon(
+                  text: "Search",
+                  icon: Icons.search_outlined,
+                  selected: false,
+                  onPressed: () {},
+                  defaultColor: secondaryColor,
+                  selectedColor: primaryColor,
+                ),
+                const SizedBox(width: 56),
+                NavBarIcon(
+                    text: "Cart",
+                    icon: Icons.local_grocery_store_outlined,
+                    selected: false,
+                    onPressed: () {},
+                    defaultColor: secondaryColor,
+                    selectedColor: primaryColor),
+                NavBarIcon(
+                  text: "Calendar",
+                  icon: Icons.date_range_outlined,
+                  selected: false,
+                  onPressed: () {},
+                  selectedColor: primaryColor,
+                  defaultColor: secondaryColor,
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BottomNavCurvePainter extends CustomPainter {
+  BottomNavCurvePainter(
+      {this.backgroundColor = Colors.white, this.insetRadius = 38});
+
+  Color backgroundColor;
+  double insetRadius;
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()
+      ..color = backgroundColor
+      ..style = PaintingStyle.fill;
+    Path path = Path()..moveTo(0, 12);
+
+    double insetCurveBeginnningX = size.width / 2 - insetRadius;
+    double insetCurveEndX = size.width / 2 + insetRadius;
+    double transitionToInsetCurveWidth = size.width * .05;
+    path.quadraticBezierTo(size.width * 0.20, 0,
+        insetCurveBeginnningX - transitionToInsetCurveWidth, 0);
+    path.quadraticBezierTo(
+        insetCurveBeginnningX, 0, insetCurveBeginnningX, insetRadius / 2);
+
+    path.arcToPoint(Offset(insetCurveEndX, insetRadius / 2),
+        radius: const Radius.circular(10.0), clockwise: false);
+
+    path.quadraticBezierTo(
+        insetCurveEndX, 0, insetCurveEndX + transitionToInsetCurveWidth, 0);
+    path.quadraticBezierTo(size.width * 0.80, 0, size.width, 12);
+    path.lineTo(size.width, size.height + 56);
+    path.lineTo(0, size.height + 56);
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
+}
+
+class NavBarIcon extends StatelessWidget {
+  const NavBarIcon({
+    Key? key,
+    required this.text,
+    required this.icon,
+    required this.selected,
+    required this.onPressed,
+    this.selectedColor = const Color(0xffFF8527),
+    this.defaultColor = Colors.black54,
+  }) : super(key: key);
+
+  final String text;
+  final IconData icon;
+  final bool selected;
+  final Function() onPressed;
+  final Color defaultColor;
+  final Color selectedColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        IconButton(
+          onPressed: onPressed,
+          splashColor: Colors.transparent,
+          highlightColor: Colors.transparent,
+          icon: Icon(
+            icon,
+            size: 25,
+            color: selected ? selectedColor : defaultColor,
+          ),
+        ),
+      ],
+    );
+  }
+}
