4822a52241dce8453f473e6e893db34d7aba357e
Khubaib Jamal <khubaib.j18@gmail.com>
2024-07-02 00:02:29 +0500

    widget:simple navigation bar

--- lib/home/navigation_bars.dart
+++ lib/home/navigation_bars.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:fyp/navigation%20bar/nav_bar1.dart';
 import 'package:fyp/navigation%20bar/nav_bar2.dart';
 import 'package:fyp/navigation%20bar/nav_bar3.dart';
+import 'package:fyp/navigation%20bar/nav_bar4.dart';
 
 class NAvigationBars extends StatelessWidget {
   const NAvigationBars({super.key});
@@ -21,6 +22,7 @@ class NAvigationBars extends StatelessWidget {
               SizedBox(height: 20),
               NavBar3(),
               SizedBox(height: 20),
+              NavBar4(),
               SizedBox(height: 20),
               SizedBox(height: 20),
             ],
