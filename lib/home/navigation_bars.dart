f556fbef908801465ee2d99029d622437166461f
Khubaib Jamal <khubaib.j18@gmail.com>
2024-07-06 02:45:27 +0500

    widget:design neumorphic nav bar

--- lib/home/navigation_bars.dart
+++ lib/home/navigation_bars.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:fyp/navigation%20bar/nav_bar1.dart';
 import 'package:fyp/navigation%20bar/nav_bar10.dart';
 import 'package:fyp/navigation%20bar/nav_bar11.dart';
+import 'package:fyp/navigation%20bar/nav_bar12.dart';
 import 'package:fyp/navigation%20bar/nav_bar2.dart';
 import 'package:fyp/navigation%20bar/nav_bar3.dart';
 import 'package:fyp/navigation%20bar/nav_bar4.dart';
@@ -18,32 +19,34 @@ class NAvigationBars extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: const SingleChildScrollView(
+      body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.all(0.0),
+          padding: const EdgeInsets.all(0.0),
           child: Column(
             children: [
-              NavBar1(),
-              SizedBox(height: 20),
-              NavBar2(),
-              SizedBox(height: 20),
-              NavBar3(),
-              SizedBox(height: 20),
-              NavBar4(),
-              SizedBox(height: 20),
-              NavBar5(),
-              SizedBox(height: 20),
-              NavBar6(),
-              SizedBox(height: 20),
-              NavBar7(),
-              SizedBox(height: 20),
-              NavBar8(),
-              SizedBox(height: 20),
-              NavBar9(),
-              SizedBox(height: 20),
-              NavBar10(),
-              SizedBox(height: 20),
-              NavBar11(),
+              const NavBar1(),
+              const SizedBox(height: 20),
+              const NavBar2(),
+              const SizedBox(height: 20),
+              const NavBar3(),
+              const SizedBox(height: 20),
+              const NavBar4(),
+              const SizedBox(height: 20),
+              const NavBar5(),
+              const SizedBox(height: 20),
+              const NavBar6(),
+              const SizedBox(height: 20),
+              const NavBar7(),
+              const SizedBox(height: 20),
+              const NavBar8(),
+              const SizedBox(height: 20),
+              const NavBar9(),
+              const SizedBox(height: 20),
+              const NavBar10(),
+              const SizedBox(height: 20),
+              const NavBar11(),
+              const SizedBox(height: 20),
+              NavBar12(),
             ],
           ),
         ),
---
+++ lib/navigation bar/nav_bar12.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+
+class NavBar12 extends StatefulWidget {
+  NavBar12({
+    Key? key,
+    this.bevel = 10.0,
+  })  : blurOffset = Offset(bevel / 2, bevel / 2),
+        color = Colors.grey.shade200;
+
+  final double bevel;
+  final Offset blurOffset;
+  final Color color;
+  @override
+  State<NavBar12> createState() => _NavBar12State();
+}
+
+class _NavBar12State extends State<NavBar12> {
+  bool isPress = false;
+  @override
+  Widget build(BuildContext context) {
+    final color = widget.color;
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(widget.bevel),
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            isPress ? color : color.mix(Colors.black, 0.1),
+            isPress ? color.mix(Colors.black, 0.5) : color,
+            isPress ? color.mix(Colors.black, 0.5) : color,
+            color.mix(Colors.white, isPress ? 0.2 : 0.5),
+          ],
+          stops: const [0.0, 0.3, 0.6, 1.6],
+        ),
+        boxShadow: isPress
+            ? null
+            : [
+                BoxShadow(
+                  blurRadius: widget.bevel,
+                  offset: -widget.blurOffset,
+                  color: color.mix(Colors.white, 0.6),
+                ),
+                BoxShadow(
+                  blurRadius: widget.bevel,
+                  offset: widget.blurOffset,
+                  color: color.mix(Colors.black, 0.3),
+                ),
+              ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            NavBarIcon(
+              text: "Home",
+              icon: Icons.home_outlined,
+              selected: true,
+              onPressed: () {},
+            ),
+            NavBarIcon(
+              text: "Search",
+              icon: Icons.search_outlined,
+              selected: false,
+              onPressed: () {},
+            ),
+            NavBarIcon(
+              text: "Cart",
+              icon: Icons.local_grocery_store_outlined,
+              selected: false,
+              onPressed: () {},
+            ),
+            NavBarIcon(
+              text: "Calendar",
+              icon: Icons.date_range_outlined,
+              selected: false,
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NavBarIcon extends StatelessWidget {
+  const NavBarIcon({
+    Key? key,
+    required this.text,
+    required this.icon,
+    required this.selected,
+    required this.onPressed,
+  }) : super(key: key);
+  final String text;
+  final IconData icon;
+  final bool selected;
+  final Function() onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        IconButton(
+          onPressed: onPressed,
+          icon: Icon(
+            icon,
+            size: 25,
+            color: selected ? Colors.grey : Colors.black54,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+extension ColorUtils on Color {
+  Color mix(Color another, double amount) {
+    return Color.lerp(this, another, amount)!;
+  }
+}
